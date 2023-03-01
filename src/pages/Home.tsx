1ed3c64fa176b47de9c0aa7a06579db496c9d458
Sarah L <alaskalaska18@yandex.ru>
2023-03-01 19:10:47 +0300

    added moment, updated design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,88 @@
 import React from 'react';
 import Header from '../components/Header';
+import './home.scss'; 
+import Select from 'react-select';
+import moment from 'moment'
 
-const Home = () => {
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
+
+
+const Home = () =>  {
+
+  const [selectedOption, setSelectedOption] = React.useState(null);
+
+
+
+
   return (
     <div>
       <Header />
-    </div>
+<div className='mainPage'>
+
+<div className="containerHome">
+  <h1>Currency Converter</h1>
+
+  <h2 className="moment">{'Today ' + moment().format('Y-M-DD HH:mm')}</h2>
+
+<div className="selectersContainer">
+<div className="leftBlock">
+<Select
+className="select"
+        defaultValue={selectedOption}
+        options={options}
+      />
+
+
+
+<input placeholder='...'  type="text"/>
+</div>
+
+
+    <button className="btn" id="swap" title="Swap Country">
+    <svg width="35" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16 13V11.5H10V9.5H16V8L19 10.5L16 13Z" fill="currentColor" />
+      <path d="M8 17V15.5H14V13.5H8V12L5 14.5L8 17Z" fill="currentColor" />
+    </svg>
+  </button>
+
+
+<div className="rightBlock">
+<Select
+        defaultValue={selectedOption}
+        options={options}
+      />
+
+
+<input placeholder='...' type="text"/>
+</div>
+</div>
+
+
+
+
+
+
+    
+  </div>
+  {/* <button className="btn" id="btn">Converter</button>
+  <small id="errorMSG"></small>
+  <small id="info"></small> */}
+
+</div>
+
+</div>
+
+
+      
   );
 };
 
---
+++ src/pages/select.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+export const options  = [
+
+      { value: 'usd', text: 'American Dollar' },
+      { value: 'eur', text: 'Euro' },
+      { value: 'dzd', text: 'Algerian Dinar' },
+      { value: 'cad', text: 'Canadian Dollar' },
+      { value: 'aed', text: 'United Arab Emirates Dirham' },
+      { value: 'amd', text: 'Armenian dram' },
+      { value: 'iqd', text: 'Iraqi Dinar' },
+      { value: 'jmd', text: 'Jamaican Dollar' },
+      { value: 'jod', text: 'Jordanian Dinar' },
+      { value: 'jpy', text: 'Japanese Yen' },
+    
+]
+
+
+const Select = () => {
+
+
+
+  return (
+
+
+    <div>
+
+
+    </div>
+  )
+}
+
+export default Select
